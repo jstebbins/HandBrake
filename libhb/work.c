c96320558aa1ce0214bf9491f59627f220227d71
John Stebbins <jstebbins.hb@gmail.com>
2016-05-07 11:22:56 -0600

    work: make video decoder input fifo smaller
    This prevents audio from getting so far ahead of video

--- libhb/work.c
+++ libhb/work.c
@@ -1501,7 +1501,7 @@ static void do_job(hb_job_t *job)
     else
 #endif
     {
-        job->fifo_mpeg2  = hb_fifo_init( FIFO_LARGE, FIFO_LARGE_WAKE );
+        job->fifo_mpeg2  = hb_fifo_init( FIFO_SMALL, FIFO_SMALL_WAKE );
         job->fifo_raw    = hb_fifo_init( FIFO_SMALL, FIFO_SMALL_WAKE );
         job->fifo_sync   = hb_fifo_init( FIFO_SMALL, FIFO_SMALL_WAKE );
         job->fifo_mpeg4  = hb_fifo_init( FIFO_LARGE, FIFO_LARGE_WAKE );
